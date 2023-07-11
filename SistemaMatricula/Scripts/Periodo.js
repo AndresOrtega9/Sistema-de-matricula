bd8daae74170fd643c5681deb257592cfa8809c6
naortega <andresortega0218@gmail.com>
2023-07-11 12:52:02 -0500

    Nuevas funcionalidades

--- SistemaMatricula/Scripts/Periodo.js
+++ SistemaMatricula/Scripts/Periodo.js
@@ -25,6 +25,7 @@ function ListaDePeriodos(data) {
     contenido += "<td>NOMBRE</td>";
     contenido += "<td>FECHA DE INICIO</td>";
     contenido += "<td>FECHA DE FIN</td>";
+    contenido += "<td>OPCIONES</td>";
     contenido += "</tr>";
     contenido += "</thead>";
 
@@ -35,8 +36,13 @@ function ListaDePeriodos(data) {
         contenido += "<td>" + data[i].NOMBRE + "</td>";
         contenido += "<td>" + data[i].FECHAINICIO + "</td>";
         contenido += "<td>" + data[i].FECHAFIN + "</td>";
+        contenido += "<td>";
+        contenido += "<button class='btn btn-primary'><i class='glyphicon glyphicon-edit'></i></button> "
+        contenido += "<button class='btn btn-danger'><i class='glyphicon glyphicon-trash'></i></button>"
+        contenido += "</td>";
         contenido += "</tr>";
     }
+
     contenido += "</tbody>";
     contenido += "</table>";
     document.getElementById("tabla").innerHTML = contenido;
