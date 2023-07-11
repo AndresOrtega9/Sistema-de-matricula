a0ebc1a12c455d6851271ba0a2aa5b0c5a421f6f
naortega <andresortega0218@gmail.com>
2023-07-10 22:50:40 -0500

    Se agregó nuevo modulo, Docente

---
+++ SistemaMatricula/Controllers/DocenteController.cs
@@ -0,0 +1,62 @@
+﻿using SistemaMatricula.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaMatricula.Controllers
+{
+    public class DocenteController : Controller
+    {
+        // GET: Docente
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public JsonResult ObtenerDocentes()
+        {
+
+            SistemaMatriculaEntities db = new SistemaMatriculaEntities();
+            var lista = (from docente in db.Docente
+                         select new
+                         {
+                             docente.IIDDOCENTE,
+                             docente.NOMBRE,
+                             docente.APPATERNO,
+                             docente.APMATERNO,
+                             docente.EMAIL,
+                         }).ToList();
+
+            return Json(lista, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult LlenarComboModalidadContrato()
+        {
+            SistemaMatriculaEntities db = new SistemaMatriculaEntities();
+            var lista = db.ModalidadContrato.Where(o =>o.BHABILITADO==1)
+                .Select(o => new { o.IIDMODALIDADCONTRATO, o.NOMBRE }).ToList();
+
+            return Json(lista, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult FiltrarDocentePorTipoModalidad(int idModalidad)
+        {
+            SistemaMatriculaEntities db = new SistemaMatriculaEntities();
+            var lista = (from docente in db.Docente
+                         where docente.IIDMODALIDADCONTRATO == 1
+                         select new
+                         {
+                             docente.IIDDOCENTE,
+                             docente.NOMBRE,
+                             docente.APPATERNO,
+                             docente.APMATERNO,
+                             docente.EMAIL
+                         }).ToList();
+
+            return Json(lista, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
